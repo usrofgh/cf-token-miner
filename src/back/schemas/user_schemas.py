cdf17f1be9fe5d7aeef4fb0a31070f2576b6cb04
anon <anon@gmail.com>
2024-10-07 14:35:21 +0300

    Add min/max password restriction in the pydantic model

--- src/back/schemas/user_schemas.py
+++ src/back/schemas/user_schemas.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
 
 class UserCreateSchema(BaseModel):
     email: EmailStr
-    password: str
+    password: str = Field(min_length=8, max_length=100)
 
 class UserReadSchema(BaseModel):
     id: int
